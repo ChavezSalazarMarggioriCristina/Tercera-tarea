7f303ad94021a6154dd143e517830dbddd7ecf72
MarggioriCristinaChavezSalazar <131232181+ChavezSalazarMarggioriCristina@users.noreply.github.com>
2023-05-06 11:40:26 -0500

    Update Main.java

--- VectorResultante/src/Main.java
+++ VectorResultante/src/Main.java
@@ -2,14 +2,24 @@ public class Main
 {
     public static void main(String args[])
     {
-        VectorResultante primerVector = new VectorResultante(5,12);
-        VectorResultante segundoVector = new VectorResultante(-2,-3);
+        VectorResultante primerVector = new VectorResultante(1,2);
+        VectorResultante segundoVector = new VectorResultante(3,0);
 
-        VectorResultante sumaVectores = primerVector.suma(segundoVector);
+        VectorResultante sumaVectores = new VectorResultante(primerVector.getA(), segundoVector.getB());
+        //VectorResultante sumaVectores = primerVector.suma(segundoVector);
         double resultante = sumaVectores.obtenerResultante();
+
+        VectorResultante sumaR1 = new VectorResultante(primerVector.getA(), segundoVector.getA());
+        double suma1 = sumaR1.obtenerSuma1();
+
+        VectorResultante sumaR2 = new VectorResultante(primerVector.getB(), segundoVector.getB());
+        double suma2 = sumaR2.obtenerSuma2();
+
 
         System.out.printf("La resultante es: " + resultante);
 
-        System.out.printf('\n'+"La suma de los vectores es: " + sumaVectores);
+        System.out.printf('\n'+"La suma del vector 1 y 2 es: " + '\n' + suma1 + '\n' + suma2);
+        //System.out.printf('\n'+"La suma del vector 2 es: " + suma2);
+
     }
 }
